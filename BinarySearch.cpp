d0e680d022453343846d44e21ef5b1c94f894c10
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2020-08-13 13:34:40 +0200

    Minor changes.

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "BinarySearch.h"
-
-template <typename T>
-T* sort(T* arr) {
-    int length = sizeof(arr) / sizeof(T);
-    return arr;
+namespace BinarySearch {
+    template <typename T>
+    T* sort(T* arr, int length) {
+        return arr;
+    }
 }
+
+
+
--- testLib.cpp
+++ testLib.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "BinarySearch.cpp"
 
+using namespace BinarySearch;
+
 int main() {
-    int arr[] = {1,2,3};
-    int* sorted = sort(arr);
+    int arr[] = {1,2,3, 4};
+    int length = sizeof(arr) / sizeof(*arr);
+    int* sorted = sort(arr, length);
+
     return 0;
 }
