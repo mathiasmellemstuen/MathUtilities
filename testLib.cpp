818585efe59ae79e1c4b6df32c7dbd87dcc875b7
Tobias Hallingstad <tobias@hallingstad.me>
2020-10-16 15:30:24 +0200

    Some reformating

--- testLib.cpp
+++ testLib.cpp
@@ -6,24 +6,28 @@
 #include "Experimental/SleepSort.h"
 
 int main() {
+    int arr[] = {5,1,54,-2,54,36, 70};
+    int length = sizeof(arr) / sizeof(*arr);
 
     //Testing the insertion sort algorithm:
     std::cout << "TESTING: Insertion sort" << std::endl;
-    int arr[] = {5,1,54,-2,54,36, 70};
-    int length = sizeof(arr) / sizeof(*arr);
     int* sorted = SortingAlgorithms::Basic::InsertionSort::sort(arr, length);
-
-    for(int i = 0; i < length; i++) {
-        std::cout << sorted[i] << std::endl;
-    }
-
+    for(int i = 0; i < length; i++)
+        std::cout << sorted[i] << " ";
+    std::cout << std::endl << std::endl;
 
     //Testing inverting algorithm:
     std::cout << "TESTING: Inverting algorithm on the previous result." << std::endl;
     int* inverted = SortingAlgorithms::Utility::InvertArray::invert(sorted, length);
-    for(int i = 0; i < length; i++) {
-        std::cout << inverted[i] << std::endl;
-    }
+    for(int i = 0; i < length; i++)
+        std::cout << inverted[i] << " ";
+    std::cout << std::endl << std::endl;
+
+    std::cout << "TESTING: Bubble sort" << std::endl;
+    int* sorted3 = SortingAlgorithms::Basic::BubbleSort::sort(arr,length);
+    for(int i = 0; i < length; i++)
+        std::cout << sorted[i] << " ";
+    std::cout << std::endl << std::endl;
 
     // Testing sleep sorting:
     std::cout << "TESTING: Timed sorting" << std::endl;
@@ -47,21 +51,13 @@ int main() {
     std::cout << "Sorted array:   ";
     for (int i = 0; i < length2; i++)
         std::cout << sorted2[i] << " ";
-    std::cout << std::endl;
-
-
+    std::cout << std::endl << std::endl;
 
     std::cout << "TESTING: Swap" << std::endl;
     int number1 = 1;
     int number2 = 2;
     SortingAlgorithms::Utility::Swap::swap(&number1, &number2);
-    std::cout << "Number 1: " << number1 << " and number 2: " << number2 << std::endl;
-
-    std::cout << "TESTING: Bubble sort" << std::endl;
-    int* sorted3 = SortingAlgorithms::Basic::BubbleSort::sort(arr,length);
-    for(int i = 0; i < length; i++) {
-        std::cout << sorted[i] << std::endl;
-    }
+    std::cout << "Number 1: " << number1 << " and number 2: " << number2 << std::endl << std::endl;
 
     delete[] inverted;
     delete[] sorted;
